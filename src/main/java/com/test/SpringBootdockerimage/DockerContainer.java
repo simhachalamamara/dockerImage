5014020a3e8e2246675a7623711562a6f1102ad2
simhachalam <simhachalamamara@gmail.com>
2021-09-29 15:44:22 +0530

    Update DockerContainer.java
    committed changes

--- src/main/java/com/test/SpringBootdockerimage/DockerContainer.java
+++ src/main/java/com/test/SpringBootdockerimage/DockerContainer.java
@@ -1,7 +1,4 @@
 package com.test.SpringBootdockerimage;
-
-import javax.xml.ws.RespectBinding;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

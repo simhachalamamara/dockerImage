999467d57da64feff149995085f85f079a3e0e08
simhachalam <simhachalamamara@gmail.com>
2021-10-06 09:48:00 +0530

    Update SpringBootDockerImageApplicationTests.java

--- src/test/java/com/test/SpringBootdockerimage/SpringBootDockerImageApplicationTests.java
+++ src/test/java/com/test/SpringBootdockerimage/SpringBootDockerImageApplicationTests.java
@@ -2,12 +2,22 @@ package com.test.SpringBootdockerimage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class SpringBootDockerImageApplicationTests {
+	
+	@Autowired
+	private DockerContainer dockerContainer;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void getString()
+	{
+		String message=dockerContainer.getHello();
+		assertEquals("hello from SpringBoot",message);
 	}
 
 }
